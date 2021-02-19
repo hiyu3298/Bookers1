234cb6fe09ac1bc30983f30f725a008d65bef81c
hiyu3298 <akamatsu11218991@gmail.com>
2021-02-19 06:00:21 +0000

    [Add] 編集機能の追加

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -12,12 +12,19 @@ class BooksController < ApplicationController
   end
 
   def create
-    @book = Book.new(book_params)
-    @book.save
-    redirect_to books_path(@book.id)
+    book = Book.new(book_params)
+    book.save
+    redirect_to book_path(book.id)
   end
 
   def edit
+    @book = Book.find(params[:id])
+  end
+
+  def update
+    book = Book.find(params[:id])
+    book.update(book_params)
+    redirect_to book_path(book)
   end
 
   private
