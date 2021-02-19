5ca6eda6c9b6e2d6ce2d8b0a4a845a5f72e1776c
hiyu3298 <akamatsu11218991@gmail.com>
2021-02-19 09:23:31 +0000

    [Update] 本来のサイトに近い形に整えた

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -14,8 +14,13 @@ class BooksController < ApplicationController
 
   def create
     book = Book.new(book_params)
-    book.save
+    if book.save
+    flash[:notice] = "Book was successfully created."
     redirect_to book_path(book.id)
+    else
+    @books = Book.all
+    render :index
+    end
   end
 
   def edit
@@ -24,13 +29,18 @@ class BooksController < ApplicationController
 
   def update
     book = Book.find(params[:id])
-    book.update(book_params)
-    redirect_to book_path(book)
+    if book.update(book_params)
+      flash[:notice] = "Book was successfully updated."
+      redirect_to book_path(book)
+    else
+      render :edit
+    end
   end
-  
+
   def destroy
     book = Book.find(params[:id])
     book.destroy
+    flash[:notice] = "Book was successfully destroyed."
     redirect_to books_path
   end
 
